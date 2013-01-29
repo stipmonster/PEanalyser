81e7dc9f37743b55a1eccd41c4bbae48b9cb14ec
Erik Kooistra <my@erikkooistra.nl>
2013-01-29 19:38:07 +0100

    new format

--- main.py
+++ main.py
@@ -1,41 +1,13 @@
-#!/usr/bin/python
-
-#import sys
-#import os
-#print 'Number of arguments:', len(sys.argv), 'arguments.'
-#print 'Argument List:', str(sys.argv)
 import os
 import sys
-import subprocess
-from multiprocessing import Process, Manager
 
-
-def printinfo(check):
-    for info in check:
-        if info[0] != '':
-            print info[1] + "\t",
-    print
-    
-    
-    
-rootdir = sys.argv[1]
-print "filename\tASLR\tHigh entropy ASLR\tDEP/NX\tSEH\tStack cookies (EXPERIMENTAL)\t"
-for root, subFolders, files in os.walk(rootdir):
-    for filename in files:
-        if  "exe" in filename or "dll" in filename:
-            filePath = os.path.join(root, filename)
-            process = subprocess.Popen(["/usr/bin/pesec","-f","csv",filePath], stdout=subprocess.PIPE)
-            
-            #exit_code = os.waitpid(process.pid, 0)
-            output=process.communicate()[0]
-            data=output.split("\n")
-            if "ASLR" in output:
-                final =[]
-                for line in data:
-                    if line =="Certificates":
-                        break
-                    temp=line.split(',')
-                    final.append(temp)
-                print filename + "\t",
-                printinfo(final)
-            
+print "/Users/Erik/sources/tor/Tor Browser/App/tor.exe"
+print "========================================================="
+path=os.path.join(os.path.dirname(os.path.abspath(__file__))+ "/plugins")
+sys.path.append(path)
+for file in os.listdir(path):
+    if file.split(".").count("py"):
+        file2= file.split(".")[0]
+        test = __import__(file2)
+        test.start("/Users/Erik/sources/tor/Tor Browser/App/tor.exe")
+        print "========================================================="
